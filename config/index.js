7fafe41bff05bc1a2ee250282d5b13bc7b3af74a
sabago <sandra.abago@gmail.com>
2020-11-15 14:41:57 -0500

    restarting

--- config/index.js
+++ config/index.js
@@ -20,6 +20,4 @@ const config = configObj[env];
 
 // const env = process.env.NODE_ENV || 'development'
 // const config = require('./' + env)
-
-configObj.logger = Logger.new(STDOUT);
 module.exports = configObj
